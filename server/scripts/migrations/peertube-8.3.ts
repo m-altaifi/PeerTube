27a7cfd94defcb3098363b024b76f997ff67a7ac
Chocobozzz <me@florianbigard.com>
2026-08-11 10:14:54 +0200

    Use manual migration script for slow migrations

--- server/scripts/migrations/peertube-8.3.ts
+++ server/scripts/migrations/peertube-8.3.ts
@@ -9,6 +9,7 @@ run()
 
 async function run () {
   await fillVideoSearchTable()
+  await migrateVideoInfohashes()
 }
 
 async function fillVideoSearchTable () {
@@ -24,4 +25,42 @@ async function fillVideoSearchTable () {
   `)
 
   console.log('videoSearch table filled.\n')
+}
+
+async function migrateVideoInfohashes () {
+  const videoFileColumns = await sequelizeTypescript.getQueryInterface().describeTable('videoFile')
+  if (!videoFileColumns['infoHash']) {
+    console.log('Video infohashes already migrated, skipping.\n')
+    return
+  }
+
+  console.log('Migrating video infohashes...')
+
+  // Don't re-hydrate video streaming playlists infohash, PeerTube will re-generate them on the next startup
+
+  // Classic infohash are stored as 40 char hex on the video file
+  await sequelizeTypescript.query(`
+    CREATE OR REPLACE FUNCTION safe_decode_hex(input text)
+    RETURNS bytea AS $$
+    BEGIN
+      RETURN decode(input, 'hex');
+    EXCEPTION
+      WHEN others THEN
+        RETURN NULL;
+    END;
+    $$ LANGUAGE plpgsql IMMUTABLE;
+  `)
+
+  await sequelizeTypescript.query(
+    `INSERT INTO "videoInfohash" ("infohash", "videoFileId") ` +
+      `SELECT safe_decode_hex("infoHash"), "id" FROM "videoFile" WHERE "infoHash" IS NOT NULL AND safe_decode_hex("infoHash") IS NOT NULL`
+  )
+
+  await sequelizeTypescript.query(`DROP FUNCTION safe_decode_hex(text);`)
+
+  // Dropping a column also drops indexes that only depend on it (the old GIN index, and "videoFile"."infoHash")
+  await sequelizeTypescript.query(`ALTER TABLE "videoStreamingPlaylist" DROP COLUMN IF EXISTS "p2pMediaLoaderInfohashes"`)
+  await sequelizeTypescript.query(`ALTER TABLE "videoFile" DROP COLUMN IF EXISTS "infoHash"`)
+
+  console.log('Video infohashes migrated.\n')
 }
